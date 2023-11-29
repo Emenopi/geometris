5445a1f0f069c98b43f41b653d0393b80367896f
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2023-11-29 19:49:50 +0000

    First commit

---
+++ desktop/src/com/mygdx/geometris/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.geometris;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.geometris.Geometris;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Geometris");
+		new Lwjgl3Application(new Geometris(), config);
+	}
+}
