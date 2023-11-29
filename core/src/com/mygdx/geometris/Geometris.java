6bdf03420983a2abfe1a2504df4ff7afe12a5f96
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2023-11-29 20:38:15 +0000

    show boundary circle

--- core/src/com/mygdx/geometris/Geometris.java
+++ core/src/com/mygdx/geometris/Geometris.java
@@ -1,31 +1,44 @@
-package com.mygdx.geometris;
+package mygdx.geometris;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Geometris extends ApplicationAdapter {
+public class Geometris extends Game {
 	SpriteBatch batch;
-	Texture img;
+	Texture boundaryCircle;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		boundaryCircle = new Texture("circle.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		float boundaryCircleWidth = 819;
+		float boundaryCircleHeight = 819;
+		float screenWidth = Gdx.graphics.getWidth();
+		float screenHeight = Gdx.graphics.getHeight();
+		
+		int boundaryCircleBorderX = (int) ((screenWidth - boundaryCircleWidth) /2);
+		int boundaryCircleBorderY = (int) ((screenHeight - boundaryCircleHeight) /2);
+		ScreenUtils.clear(0, 0, 0, 0);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(boundaryCircle, boundaryCircleBorderX, boundaryCircleBorderY, boundaryCircleWidth, boundaryCircleHeight);
 		batch.end();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		boundaryCircle.dispose();
 	}
 }
--- desktop/src/com/mygdx/geometris/DesktopLauncher.java
+++ desktop/src/com/mygdx/geometris/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.mygdx.geometris;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.geometris.Geometris;
+import mygdx.geometris.Geometris;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Geometris");
+		config.setWindowedMode(900, 900);
 		new Lwjgl3Application(new Geometris(), config);
 	}
 }
