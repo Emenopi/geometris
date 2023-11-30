61db385da249d6b9afcab8d0b5fc35f20a09364d
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2023-11-30 11:09:34 +0000

    [GTM00b] - Render Inner Circle

--- core/src/com/mygdx/geometris/Geometris.java
+++ core/src/com/mygdx/geometris/Geometris.java
@@ -2,18 +2,24 @@ package mygdx.geometris;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Geometris extends Game {
 	SpriteBatch batch;
 	Texture boundaryCircle;
+	ShapeRenderer innerCircle;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		boundaryCircle = new Texture("circle.png");
+		
+		innerCircle = new ShapeRenderer();
 	}
 
 	@Override
@@ -22,13 +28,24 @@ public class Geometris extends Game {
 		float boundaryCircleHeight = 819;
 		float screenWidth = Gdx.graphics.getWidth();
 		float screenHeight = Gdx.graphics.getHeight();
+		float screenCentre = screenWidth / 2;
+		
+		double boundaryCircleInnerRadius = (boundaryCircleWidth/2) * 0.88;
+		float innerCircleRadius = (float) (boundaryCircleInnerRadius/4);
 		
 		int boundaryCircleBorderX = (int) ((screenWidth - boundaryCircleWidth) /2);
 		int boundaryCircleBorderY = (int) ((screenHeight - boundaryCircleHeight) /2);
+		
+		
 		ScreenUtils.clear(0, 0, 0, 0);
 		batch.begin();
 		batch.draw(boundaryCircle, boundaryCircleBorderX, boundaryCircleBorderY, boundaryCircleWidth, boundaryCircleHeight);
 		batch.end();
+		
+		innerCircle.setColor(Color.FIREBRICK);
+		innerCircle.begin(ShapeType.Filled);
+		innerCircle.circle(screenCentre, screenCentre, innerCircleRadius);
+		innerCircle.end();
 	}
 	
 	@Override
