281c63fe5c8728dc7a44d09148b37b01fd215e01
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-05-06 16:12:11 +0100

    commit all so junit doesn't fuckup again

---
+++ core/src/main/geometris/tests/LogInTest.java
@@ -0,0 +1,12 @@
+package main.geometris.tests;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogInTest {
+
+    @Test
+    void getPlayer() {
+
+    }
+}
