fbf1e85509b967a46074c49fc6513aa7eb3f19c4
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-05-06 16:38:09 +0100

    all login tests

--- core/src/main/geometris/tests/LogInTest.java
+++ core/src/main/geometris/tests/LogInTest.java
@@ -7,10 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class LogInTest {
 
     @Test
-    void getPlayer() {
+    void getPlayerNameFromValidEmail() {
         LogIn login = new LogIn();
         String email = "matthew.barr@glasgow.ac.uk";
         assertEquals(login.getPlayer(email).getName(), "Matt Barr");
 
+    }
+    @Test
+    void correctCredentialsLoginSuccessful(){
+        LogIn login = new LogIn();
+        String email = "derek.somerville@glasgow.ac.uk";
+        String password = "1234";
+        login.logIn(email, password);
+        assertEquals(login.getLoginStatus(), LogIn.Status.LOGGED_IN);
+    }
+
+    @Test
+    void unknownUserLoginChangesStatusToRegister() {
+        LogIn login = new LogIn();
+        String email = "test@test.com";
+        String password = "1234";
+        login.logIn(email, password);
+        assertEquals(login.getLoginStatus(), LogIn.Status.REGISTER);
+    }
+
+    @Test
+    void incorrectPasswordLoginChangesStatusToRetryPassword() {
+        LogIn login = new LogIn();
+        String email = "derek.somerville@glasgow.ac.uk";
+        String password = "1111";
+        login.logIn(email, password);
+        assertEquals(login.getLoginStatus(), LogIn.Status.RETRY_PASSWORD);
     }
 }
