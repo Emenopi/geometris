9edaa918636affa919f9c9f80ab5986b1d3ae431
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-05-06 16:13:43 +0100

    first test

--- core/src/main/geometris/tests/LogInTest.java
+++ core/src/main/geometris/tests/LogInTest.java
@@ -1,4 +1,5 @@
 package main.geometris.tests;
+import main.geometris.LogIn;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,9 @@ class LogInTest {
 
     @Test
     void getPlayer() {
+        LogIn login = new LogIn();
+        String email = "matthew.barr@glasgow.ac.uk";
+        assertEquals(login.getPlayer(email).getName(), "Matt Barr");
 
     }
 }
