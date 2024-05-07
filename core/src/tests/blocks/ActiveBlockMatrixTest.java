d133f5783eda168853402dae62c1a552fbc92aab
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2024-05-07 17:05:01 +0100

    add activeBlockMatrix rotation tests

--- core/src/tests/blocks/ActiveBlockMatrixTest.java
+++ core/src/tests/blocks/ActiveBlockMatrixTest.java
@@ -1,5 +1,6 @@
 package tests.blocks;
 
+import main.geometris.blocks.Matrix;
 import main.geometris.game.GameScreen;
 import main.geometris.game.Geometris;
 import main.geometris.blocks.ActiveBlockMatrix;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.spy;
@@ -27,21 +30,21 @@ class ActiveBlockMatrixTest {
         initMatrix = new Block[3][3];
     }
     @Test
-    public void whenColourIsCyan_thenMatrixIsCyanBlockShape() {
+    void whenColourIsCyan_thenMatrixIsCyanBlockShape() {
         activeBlockMatrix = spy(new ActiveBlockMatrix("CYAN", gameScreen));
         String[][] purpleBlock = new String[][]{{"CYAN", "NULL", "NULL"}, {"NULL", "NULL", "NULL"}, {"NULL", "NULL", "NULL"}};
         assertArrayEquals(purpleBlock, activeBlockMatrix.getMatrixString());
     }
 
     @Test
-    public void whenColourIsPurple_thenMatrixIsPurpleLineShape() {
+    void whenColourIsPurple_thenMatrixIsPurpleLineShape() {
         activeBlockMatrix = spy(new ActiveBlockMatrix("PURPLE", gameScreen));
         String[][] purpleBlock = new String[][]{{"PURPLE", "NULL", "NULL"}, {"PURPLE", "NULL", "NULL"}, {"PURPLE", "NULL", "NULL"}};
         assertArrayEquals(purpleBlock, activeBlockMatrix.getMatrixString());
     }
 
     @Test
-    public void whenColourIsMagenta_thenMatrixIsMagentaSquareShape() {
+    void whenColourIsMagenta_thenMatrixIsMagentaSquareShape() {
         Block[][] init = new Block[3][3];
         activeBlockMatrix = new ActiveBlockMatrix("MAGENTA", gameScreen);
         String[][] magentaBlock = new String[][]{{"MAGENTA", "MAGENTA", "NULL"}, {"MAGENTA", "MAGENTA", "NULL"}, {"NULL", "NULL", "NULL"}};
@@ -49,23 +52,39 @@ class ActiveBlockMatrixTest {
     }
 
     @Test
-    public void whenColourIsOrange_thenMatrixIsOrangeShape() {
+    void whenColourIsOrange_thenMatrixIsOrangeShape() {
         activeBlockMatrix = new ActiveBlockMatrix("ORANGE", this.gameScreen);
         String[][] orangeBlock = new String[][]{{"ORANGE", "NULL", "NULL"}, {"ORANGE", "ORANGE", "NULL"}, {"NULL", "NULL", "NULL"}};
         assertArrayEquals(orangeBlock, activeBlockMatrix.getMatrixString());
     }
 
     @Test
-    public void whenColourIsYellow_thenMatrixIsYellowShape() {
+    void whenColourIsYellow_thenMatrixIsYellowShape() {
         activeBlockMatrix = new ActiveBlockMatrix("YELLOW", gameScreen);
         String[][] yellowBlock = new String[][]{{"YELLOW", "YELLOW", "NULL"}, {"YELLOW", "NULL", "NULL"}, {"YELLOW", "NULL", "NULL"}};
         assertArrayEquals(yellowBlock, activeBlockMatrix.getMatrixString());
     }
 
     @Test
-    public void whenColourIsGreen_thenMatrixIsGreenShape() {
+    void whenColourIsGreen_thenMatrixIsGreenShape() {
         activeBlockMatrix = new ActiveBlockMatrix("GREEN", gameScreen);
         String[][] greenBlock = new String[][]{{"GREEN", "NULL", "NULL"}, {"GREEN", "GREEN", "NULL"}, {"GREEN", "NULL", "NULL"}};
         assertArrayEquals(greenBlock, activeBlockMatrix.getMatrixString());
+    }
+
+    @Test
+    void whenRotateMatrixClockWise_thenMatrixRotatedClockwise() {
+        activeBlockMatrix = new ActiveBlockMatrix("GREEN", gameScreen);
+        String[][] greenBlock = new String[][]{{"GREEN", "GREEN", "GREEN"}, {"NULL", "GREEN", "NULL"}, {"NULL", "NULL", "NULL"}};
+        Matrix.colour[][] rotatedMatrix = activeBlockMatrix.rotateClockwise();
+        assertEquals(Arrays.deepToString(greenBlock), Arrays.deepToString(rotatedMatrix));
+    }
+
+    @Test
+    void whenRotateMatrixAntilockWise_thenMatrixRotatedAntilockwise() {
+        activeBlockMatrix = new ActiveBlockMatrix("GREEN", gameScreen);
+        String[][] greenBlock = new String[][]{{"NULL", "GREEN", "NULL"}, {"GREEN", "GREEN", "GREEN"}, {"NULL", "NULL", "NULL"}};
+        Matrix.colour[][] rotatedMatrix = activeBlockMatrix.rotateAntiClockwise();
+        assertEquals(Arrays.deepToString(greenBlock), Arrays.deepToString(rotatedMatrix));
     }
 }
